4e5a2a93bf506b9a72074d3ffc0103fc9cd098e1
whoisanku <newmew.ankit@gmail.com>
2023-08-14 18:33:56 +0545

    final version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Scrappy
-This python script helps to get all the links &amp; datas inside tags for any website having no-login feature!
+
+This python script fetches all the contents from the website under same domain going through different routes & save it into a single pdf file.
+
+## Installation Guide
+
+### Step 1:
+
+Make sure to install pip package like Selenium, FPDF and beautiful soup.
+
+### Step 2:
+
+Assign the chrome driver path correctly in code in Scrappy.py in line no. 15 on the basis of your local machine.
+
+### Step 3:
+
+Run the code by typing `python3 Scrappy.py` & enter the url which you want to scrape the contents.
+
+#### Now you're contents will be stored in pdf file in the root folder. Thanks!
--- Scrappy.py
+++ Scrappy.py
@@ -1,26 +1,66 @@
-from urllib.request import Request, urlopen
-import urllib.parse, urllib.error
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-import ssl
-import re
+from fpdf import FPDF
+from urllib.parse import urlparse, urljoin
 
-# Ignore SSL certificate errors
-ctx = ssl.create_default_context()
-ctx.check_hostname = False
-ctx.verify_mode = ssl.CERT_NONE
+# Configure Chrome Options
+chrome_options = Options()
+chrome_options.headless = True  # Run in headless mode
 
-url = input("Enter - ")
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-}
-req = Request(url, headers=headers)
-html = urlopen(req, context=ctx).read()
-soup = BeautifulSoup(html, "html.parser")
+# Initialize the Chrome driver
+# You need to assign the path on the basis of your local machine
+service = Service('/Users/ankitbhandari/Scrappy/chromedriver')
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Retrieve all of the anchor tags
-tags = soup("a")
-url_pattern = re.compile(r"https?://\S+|www\.\S+")
-for tag in tags:
-    hrefData = tag.get("href", None)
-    if hrefData and url_pattern.match(hrefData):
-        print(hrefData)
+# Open the landing page
+url = input("Enter URL- ")
+parsed_input_url = urlparse(url)
+domain = parsed_input_url.netloc
+print(f"Accessing: {url}")
+driver.get(url)
+
+# Use BeautifulSoup to parse the page and extract all links
+soup = BeautifulSoup(driver.page_source, 'html.parser')
+all_links = [a['href'] for a in soup.find_all('a', href=True)]
+
+# Correct relative links and filter for same domain links
+links = []
+for link in all_links:
+    absolute_link = urljoin(url, link)
+    if urlparse(absolute_link).netloc == domain:
+        links.append(absolute_link)
+
+all_texts = []
+
+for link in links:
+    try:
+        print(f"Accessing: {link}")
+        driver.get(link)
+        page_text = driver.find_element(By.TAG_NAME, 'body').text
+        all_texts.append(page_text)
+    except Exception as e:
+        print(f"Failed to access: {link}. Error: {e}")
+
+driver.quit()
+
+for text in all_texts:
+    print(text)
+
+# Create a new PDF
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial", size = 12)
+
+# Add each extracted text to the PDF
+for text in all_texts:
+    text = text.encode('utf-8').decode('latin-1', 'replace')
+    pdf.multi_cell(0, 10, txt=text, border=0, align='L')
+
+# Save the PDF to a file
+pdf_output_path = "/Users/ankitbhandari/Scrappy/extracted_text.pdf"
+pdf.output(pdf_output_path)
+
+print(f"Text has been saved to: {pdf_output_path}")
